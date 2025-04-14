aac40e645a2764658d2c49b4fda8e319c9bca46a
Rakshit <kashyap.rakshithjv@gmail.com>
2025-04-14 12:52:20 +0530

    Update Scrappy-Doo.py

--- Scrappy-Doo.py
+++ Scrappy-Doo.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain import LLMChain, PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
